f345372162b3be816d55291fc9c0a5676a92e9bb
Liben G/medhin <149259472+libengebremedhin@users.noreply.github.com>
2024-03-01 08:45:24 +0300

    Update models.py

--- BlogApp/models.py
+++ BlogApp/models.py
@@ -1,10 +1,3 @@
-# In the "BlogApp" app, extend the "Post" model with the following fields and constraints:
-# Title (CharField, unique=True)
-# Content (TextField)
-# Category (CharField)
-# Image (ImageField)
-# Tags (ArrayField(CharField))
-
 from django.db import models
 
 
@@ -20,6 +13,5 @@ class Post(models.Model):
     category = models.CharField(max_length=50)
     image = models.ImageField(upload_to='blog/')
     tags = models.ManyToManyField(Tag)
-    # You guys mentioned that this tags model should be an ArrayField, but when I used it, it was saying it only works on a PostgreSQL database. So, I created another model called 'tags' and included it.
     def __str__(self):
         return self.title
